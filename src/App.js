746908b0dd8516842014c733c2a5bb8bcbb2a6cf
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-01-13 14:22:45 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-
+import { useState } from 'react';
 import './App.css';
+import text from './data';
 
 function App() {
+  const [num, setNum] = useState(0)
+  const [paragraphs, setParagraphs] = useState([])
+
+  const handleNum = (e)=>{
+    setNum(e.target.value)
+  }
+
+  const handlePara = (e) =>{
+    e.preventDefault()
+    let amt = num
+    if(num <= 0){
+      amt = 0
+    }
+    setParagraphs(text.slice(0,amt))
+  }
   return (
     <div className="App">
-      Parent
+      <h3>Lorem Ipsum Generator</h3>
+      <form onSubmit={handlePara}>
+        <label htmlFor='amount'>Paragraphs: </label>
+        <input
+        type='number'
+        name='amount'
+        id='amount'
+        value={num}
+        onChange={handleNum} 
+        />
+        <button>
+          Generate
+        </button>
+      </form>
+      <article>
+        {paragraphs.map((para,index)=>
+          <p key={index}>
+            {para}
+          </p>
+      )}
+      </article>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const text = [
+    `They said the can but they can't, the court was in court of course yes no
+    we the people of not but nothing permanently with will do the right wront things
+    the same can will small practice when not suit up in the collapse in theatre but 
+    not too good.`,
+    `Sweet and cake are not so so people on the edge of the wrong road, maybe the are not
+    exceesie but food is not what it cricket in the jungle, caring for adding the contrete 
+    but cement is short on the long run but kicking on the line in the sweet corn of chocolate
+    to nice but bad is not ugly. `,
+    `The people of the village people in the jungle, for for crazy in the missing of the judge 
+    bought but loss is cool in the morning of englis not coolt but tey is good, yes may luck no 
+    luck try is on chocolate and cheese not but great on the hear nose no hear english cool 
+    bitter not bitter.`,
+    `Trust easy on the corner of conner, let's not be on crying of the sweet sweet money on the 
+    senseles of crazy in the english is the cool not indecisive of permant but damage is non
+    of the people on the police continue the same, I see but not really cry and see the mountain
+    not really but great. `,
+    `Sing no one said in the the ground, referee cheese on the ground wit on the knowledge headache
+    swim the pool shore people dream sort on the cool and heat heart cute, same in the view sweep the 
+    ground on the flag no view in the era danger but see for the hear and the eyes, white numbers are
+    not shown on. `
+]
+
+export default text
